95c3dfddc9c778500b7a458cc4c003df922827c6
Dionork <rodya.ru@inbox.ru>
2023-02-10 17:01:07 +0700

    Добавил методы вставки и удаления в CreateMass

--- simpleSort/CreateMass.java
+++ simpleSort/CreateMass.java
@@ -2,22 +2,41 @@ package simpleSort;
 
 public class CreateMass {
     private int[] array;
+    private int nElements;
 
     public CreateMass(int max) {
         array = new int[max];
-        array = setArray (max);
-        }
-
-    private int[] setArray(int max) {
-        for (int i=0;i<max;i++) {
-            array [i] = (int) (Math.random() * 100);
-        }
-        return array;
+        nElements = 0;
     }
-    public void displayArr (){
-        for (int x: array) {
-            System.out.print(" "+x);
+
+
+    public void displayArr() {
+        for (int i = 0; i < nElements; i++) {
+            System.out.print(" " + array[i]);
         }
         System.out.println("\n");
+
     }
+
+    public void delete(int val) {
+        int j;
+        for (j = 0; j < nElements; j++) {
+            if (val == array[j]) {
+                for (int i = j; i < nElements; i++) {
+                    array[i] = array[i + 1];
+                    nElements--;
+                    System.out.println("Значение удалено!");
+                    return;
+                }
+            }
+        }
+        System.out.println("Значение не найдено!");
+    }
+
+    public void insert(int val) {
+        array[nElements] = val;
+        nElements++;
+    }
+
+
 }
--- simpleSort/SimpleSortMain.java
+++ simpleSort/SimpleSortMain.java
@@ -2,9 +2,16 @@ package simpleSort;
 
 public class SimpleSortMain {
     public static void main(String[] args) {
-    CreateMass firstMass = new CreateMass(20);
-    CreateMass secondMass = new CreateMass(20);
+    CreateMass firstMass = new CreateMass(100);
+        for (int i = 0; i < 10; i++) {
+            firstMass.insert((int)(Math.random()*100));
+        }
+
+        firstMass.insert(20);
+
         firstMass.displayArr();
-        secondMass.displayArr();
+        firstMass.delete(20);
+        firstMass.displayArr();
+
     }
 }
